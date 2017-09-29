7cf19a3ea44696dea137d46b65600b3533be9fe0
drkrieger <eplyushko@gmail.com>
2017-09-29 15:21:00 -0700

    query half made

---
+++ Lam15Demo/Lam15Demo/ViewModels/MovieGenreViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Lam15Demo.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lam15Demo.ViewModel
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> movie;
+        public SelectList genres; 
+
+
+    }
+}
